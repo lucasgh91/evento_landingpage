5fe15bd051d1d0b8f3f0f06691319768faa72082
Lucas G. <ghizzolucas@gmail.com>
2024-10-20 16:50:59 -0300

    Criado: Header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,31 @@
-console.log('Hello World!');
+window.addEventListener('DOMContentLoaded', function() {
+
+  const dataEvento = new Date('Jun 27, 2025 00:00:00')
+
+  const atualizaAsHoras = this.setInterval(function() {
+
+    const dataAtual = new Date()
+    const diferencaDasHoras = dataEvento.getTime() - dataAtual.getTime()
+
+    const tempoEmMs = {
+      dias: 1000*60*60*24,
+      horas: 1000*60*60,
+      minutos: 1000*60,
+      segundos: 1000
+    }
+
+    const dias = Math.floor(diferencaDasHoras / (1000 * 60 * 60 * 24))
+    const horas = Math.floor((diferencaDasHoras % tempoEmMs.dias) / tempoEmMs.horas)
+    const minutos = Math.floor((diferencaDasHoras % tempoEmMs.horas) / tempoEmMs.minutos)
+    const segundos = Math.floor((diferencaDasHoras % tempoEmMs.minutos) / tempoEmMs.segundos)
+
+    if (dias < 0) {
+      clearInterval(atualizaAsHoras)
+      document.getElementById('timeToEvent').parentNode.innerHTML = `O evento foi finalizado há <span id="timeToEvent">${Math.abs(dias)}</span> dias.`
+    } else {
+      document.getElementById('timeToEvent').innerHTML = `${dias}d ${horas}h ${minutos}m ${segundos}s`
+    }
+
+  }, 1000)
+
+})
